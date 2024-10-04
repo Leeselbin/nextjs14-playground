c32672f5ae306d2c34580c3cba6a935da517e6a6
seulbin <84565641+Leeselbin@users.noreply.github.com>
2024-10-04 13:17:09 +0900

    병렬 데이터패칭

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,17 @@
-type Props = {
-    params: { id: string };
-};
+import { API_URL } from '../../../(home)/page';
 
-export default function MovieDetail(props: Props) {
-    return <h1>Movie {props.params.id}</h1>;
+async function getMovie(id: string) {
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id: string) {
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+    return <h1>{movie.title}</h1>;
 }
