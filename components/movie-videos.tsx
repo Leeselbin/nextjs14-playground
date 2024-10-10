419a65c083128ae0f61c4366cebe8953b6dfd612
seulbin <84565641+Leeselbin@users.noreply.github.com>
2024-10-10 12:19:49 +0900

    상세 스타일링완료

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-videos.module.css';
 
 async function getVideos(id: string) {
     const response = await fetch(`${API_URL}/${id}/videos`);
@@ -9,7 +10,7 @@ export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id);
 
     return (
-        <div>
+        <div className={styles.container}>
             {videos.map((video) => (
                 <iframe
                     key={video.id}
